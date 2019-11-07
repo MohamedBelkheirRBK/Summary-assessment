cc4890fb896a703e6ade0acec8741fbb4d91edbf
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-11-07 09:25:29 +0100

    finished repeatString function

--- summary-assessment.js
+++ summary-assessment.js
@@ -115,7 +115,9 @@ function each(coll, f) {
   //repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
   
   function repeatString(str, count) { 
-   // TODO: your code here 
+    if(count<= 0)
+      return '';
+    return  str+ repeatString(str, count-1)
   } 
    
   
